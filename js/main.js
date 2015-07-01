81d1b71206350c814daba9fe26b453880ddcecf7
Kimberly Yi <yikim01@gmail.com>
2015-07-01 15:39:43 -0700

    setting up blog repository

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var activateClass = document.getElementById('activateClass');
+var lines = document.getElementsByClassName('line');
+
+activateId.onclick = function(){
+activateClass.onclick = function() {
+  lines[0].innerHTML = "Good Try";
+  lines[1].innerHTML = "Javascript";
+  lines[2].innerHTML = "is too simple,";
+  lines[3].innerHTML = "that it's complicated";
+};
+}
